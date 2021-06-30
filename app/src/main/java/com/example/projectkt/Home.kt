491953ce9120f4bb895e22f0ff8e68b220653e78
tanveerhasan <tanveer.cse01@gmail.com>
2021-06-30 12:30:37 +0600

    get String, intent

---
+++ app/src/main/java/com/example/projectkt/Home.kt
@@ -0,0 +1,23 @@
+package com.example.projectkt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectkt.databinding.ActivityHomeBinding
+import com.example.projectkt.databinding.ActivityMainBinding
+
+class Home : AppCompatActivity() {
+
+    private lateinit var binding :ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        actionBar?.title="Home"
+
+
+
+    }
+}
--- app/src/main/java/com/example/projectkt/MainActivity.kt
+++ app/src/main/java/com/example/projectkt/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.projectkt
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.example.projectkt.databinding.ActivityMainBinding
 import java.util.*
 
@@ -22,8 +26,53 @@ class MainActivity : AppCompatActivity() {
         binding.btLogin.setOnClickListener(View.OnClickListener {
 
 
+       var phone = binding.etPhone.text.toString();
+       var password = binding.etPassword.text.toString();
+
+            if (binding.etPhone.text.toString().isEmpty())
+            {
+                binding.etPhone.error="Phone required"
+
+            }else if (binding.etPassword.text.toString().isEmpty())
+            {
+                binding.etPassword.error="Password required"
+            }else{
+
+                val intent = Intent(this,Home::class.java);
+                intent.putExtra("name", binding.etName.text.toString())
+                startActivity(intent);
+
+
+
+            }
+
 
         })
+
+        binding.etPassword.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                if (p0.toString().length<8)
+                {
+                    binding.etPassword.error="Week password"
+                }else if (p0.toString().isEmpty())
+                {
+                    binding.etPassword.error = "Password required"
+                }
+
+
+            }
+        })
+
+
 
 
 
