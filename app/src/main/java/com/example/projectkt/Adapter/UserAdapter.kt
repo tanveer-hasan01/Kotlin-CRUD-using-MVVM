c5323c6d01ddaec36b84ce21ffbafb0f25fdca78
tanveerhasan <tanveer.cse01@gmail.com>
2021-08-03 11:25:26 +0600

    add conditional change in item view

--- app/src/main/java/com/example/projectkt/Adapter/UserAdapter.kt
+++ app/src/main/java/com/example/projectkt/Adapter/UserAdapter.kt
@@ -3,10 +3,12 @@ package com.example.projectkt.Adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projectkt.ModelData.User
 import com.example.projectkt.R
 import kotlinx.android.synthetic.main.item_recycler_view.view.*
+import kotlin.coroutines.coroutineContext
 
 
 class UserAdapter:RecyclerView.Adapter<UserAdapter.MyViewHolder>() {
@@ -36,11 +38,41 @@ class UserAdapter:RecyclerView.Adapter<UserAdapter.MyViewHolder>() {
         val textViewName = view.tt_name
         val textViewStatus = view.tt_status
         val textViewGender = view.tt_gender
+        val imStatus=view.im_status
+        val imgender=view.im_gender
+
+
 
         fun bind(data : User) {
-            textViewName.text = "  "+data.name
-            textViewGender.text = "  "+data.gender
-            textViewStatus.text = "  "+data.status
+
+            val getName=data.name
+            val getGender=data.gender
+            val getStatus=data.status
+
+            textViewName.text = "  "+getName
+            textViewGender.text = "  "+getGender
+            textViewStatus.text = "  "+getStatus
+
+
+            if (data.gender.equals("male"))
+            {
+                imgender.setImageResource (R.drawable.ic_male )
+            }
+            else{
+                imgender.setImageResource (R.drawable.ic_female )
+            }
+
+
+            if (data.status.equals("active"))
+            {
+                imStatus.setImageResource (R.drawable.ic_active )
+            }
+            else{
+                imStatus.setImageResource (R.drawable.ic_disable )
+            }
+
+
+
         }
     }
 
