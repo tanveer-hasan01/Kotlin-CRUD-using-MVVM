685f5eee73d77138aefddf869fb37640af659e26
tanveerhasan <tanveer.cse01@gmail.com>
2021-08-03 15:51:25 +0600

    done inset new user

--- app/src/main/java/com/example/projectkt/MainActivity.kt
+++ app/src/main/java/com/example/projectkt/MainActivity.kt
@@ -10,12 +10,17 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.ViewModelProvider
+import com.example.projectkt.ModelData.User
+import com.example.projectkt.ModelData.UserResponse
+import com.example.projectkt.ViewModel.CreateViewModel
 import com.example.projectkt.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: CreateViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -24,25 +29,32 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        title="Add User"
 
 
-
+        initViewModel()
+        createUserObservable()
         initSpinner()
 
+        binding.btSave.setOnClickListener(View.OnClickListener {
 
-        binding.btLogin.setOnClickListener(View.OnClickListener {
 
-            when {
-                binding.etPhone.text.toString().isEmpty() -> {
-                    binding.etPhone.error="Phone required"
-                }
+            if (binding.etName.text.toString().isEmpty())
+            {
+                binding.etName.setError("invalid name")
 
-                else -> {
+            }else if (binding.etEmail.text.toString().isEmpty()){
+                binding.etEmail.setError("invalid email")
 
-                    val intent = Intent(this, ListActivity::class.java);
-                   // intent.putExtra("name", binding.etName.text.toString())
-                    startActivity(intent);
-                }
+            }
+
+            else if (!binding.radioFemale.isChecked||!binding.radioMale.isChecked){
+
+                Toast.makeText(this,"Please select gender!",Toast.LENGTH_LONG).show()
+            }
+            else{
+
+                createUser()
             }
         })
 
@@ -97,5 +109,56 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+
+    private fun createUser(){
+
+
+        var userData= User("",binding.etName.text.toString(),binding.etEmail.text.toString()
+            ,binding.spinnerStatus.selectedItem.toString(),binding.radioMale.text.toString())
+
+        viewModel.createUser(userData)
+
+    }
+
+
+
+    private fun initViewModel(){
+
+        viewModel=ViewModelProvider(this).get(CreateViewModel::class.java)
+
+
+    }
+
+    private fun createUserObservable()
+    {
+        viewModel.getCreateNewUserObservable().observe(this, androidx.lifecycle.Observer {
+
+            if (it==null)
+            {
+
+                Toast.makeText(this, "Dummy API insertion now unavailable", Toast.LENGTH_LONG).show()
+            }else{
+
+                Toast.makeText(this, "Successfully created/updated user", Toast.LENGTH_LONG).show()
+            }
+
+
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
 
 }
---
+++ app/src/main/java/com/example/projectkt/ViewModel/CreateViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.projectkt.ViewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.projectkt.ModelData.ModelUser
+import com.example.projectkt.ModelData.User
+import com.example.projectkt.ModelData.UserResponse
+import com.example.projectkt.Network.RetroInstance
+import com.example.projectkt.Network.RetroService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class CreateViewModel: ViewModel() {
+
+    lateinit var createViewModelLiveData: MutableLiveData<UserResponse?>
+
+    init {
+        createViewModelLiveData=MutableLiveData()
+    }
+
+    fun getCreateNewUserObservable():MutableLiveData<UserResponse?>{
+
+        return createViewModelLiveData
+    }
+
+  fun  createUser(user: User){
+
+      val retroInstance=RetroInstance.getRetroInstance().create(RetroService::class.java)
+      val call =retroInstance.createUser(user)
+      call.enqueue(object : Callback<UserResponse?> {
+          override fun onFailure(call: Call<UserResponse?>, t: Throwable) {
+              createViewModelLiveData.postValue(null)
+          }
+
+          override fun onResponse(call: Call<UserResponse?>, response: Response<UserResponse?>) {
+              if(response.isSuccessful) {
+                  createViewModelLiveData.postValue(response.body())
+              } else {
+                  createViewModelLiveData.postValue(null)
+              }
+          }
+      })
+
+
+      }
+
+
+
+
+
+}
