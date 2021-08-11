6f09eba9e432e6088ef2db1a012051898a4d97af
shakibuzzaman3104 <shakibuzzaman3104@gmail.com>
2021-08-11 12:08:44 +0600

    added delete menu item

--- app/src/main/java/com/example/projectkt/room_ui/AddActivity.kt
+++ app/src/main/java/com/example/projectkt/room_ui/AddActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.projectkt.R
@@ -22,6 +24,7 @@ class AddActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+
         title = "Offline DB"
 
         if (intent.getStringExtra("first_name") == null) {
@@ -32,8 +35,8 @@ class AddActivity : AppCompatActivity() {
             binding.lastName.setText(intent.getStringExtra("last_name"))
             binding.age.setText(intent.getStringExtra("age"))
             binding.add.text = "Update"
-        }
 
+        }
 
 
         mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
@@ -101,6 +104,25 @@ class AddActivity : AppCompatActivity() {
         }
 
 
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.delete_menu, menu)
+        return true
+    }
+
+
+    override  fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.delete_menu -> {
+                Toast.makeText(applicationContext, "click on setting", Toast.LENGTH_LONG).show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 
