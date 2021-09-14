72abdba93f4e5442df13dae4636b7337ba1e2766
Tanveer Hasan <43530217+tanveercse01@users.noreply.github.com>
2021-09-14 23:19:09 +0600

    Added some changes

--- app/src/main/java/com/example/projectkt/ModelData/ModelUser.kt
+++ app/src/main/java/com/example/projectkt/ModelData/ModelUser.kt
@@ -1,5 +1,5 @@
 package com.example.projectkt.ModelData
-
+// todo
 data class ModelUser(val data: List<User>)
 data class User(val id: String?,val name: String?,  val email: String?, val status: String?, val gender: String?)
 data class UserResponse(val code: Int?, val meta: String?, val data: User?)
